cd0104799f678661cfebb560cdf47fdcafe392a9
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-05 13:24:04 -0600

    Created an attributes file to hold slurm configuration

---
+++ cookbooks/slurm-mpi-cluster/attributes/slurm.rb
@@ -0,0 +1,8 @@
+slurm['master']         = "domU-12-31-38-07-4E-D6"
+slurm['master_addr']    = "50.19.174.152"
+slurm['computes']       = [ "ip-10-120-243-142", "domU-12-31-38-00-32-21" ]
+slurm['compute_addrs']  = [ "184.73.87.168", "50.17.71.36" ]
+slurm['part_name']      = "debug"
+slurm['user']           = "slurm"
+slurm['cpus']           = "1"
+
