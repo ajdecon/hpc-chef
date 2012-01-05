8f813b7f8f4b740997fc32677ef0cb3585f9f583
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-05 17:25:33 -0600

    Added a couple of config options for NFS shares to default attributes

--- cookbooks/slurm-mpi-cluster/attributes/default.rb
+++ cookbooks/slurm-mpi-cluster/attributes/default.rb
@@ -1,3 +1,4 @@
+# SLURM attributes
 default['slurm']['master']         = "ip-10-120-177-89"
 default['slurm']['master_addr']    = "107.22.142.122"
 default['slurm']['computes']       = [ "ip-10-120-243-142", "domU-12-31-38-00-32-21" ]
@@ -6,4 +7,9 @@ default['slurm']['part_name']      = "debug"
 default['slurm']['user']           = "slurm"
 default['slurm']['cpus']           = "1"
 
+# MUNGE attributes
 default['munge']['key']    = "nfusdnfjkaniuownfnljdnvcuowanrljvnjnvuoandfnsjanvuonejlnvjnuoannfdjslnvuaownljnfsdljvnsdualnfvjnafsuvnoweiunvlgrn"
+
+# NFS share attributes
+default['nfs']['headnode_addr']    = default['slurm']['master_addr']
+default['nfs']['shared_dirs']      = ["/home"]
