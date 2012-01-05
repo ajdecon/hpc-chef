832e35b9ad733ebc12194cab36dc442875198d89
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-05 13:08:16 -0600

    Troubleshooting to get slurm.rb recipe working.
     - Had to add a key and configuration for munge auth service.
    
     - Due to the networking situation on EC2, had to
    explicitly include the IP addresses of the slurm nodes rather
    than resolving hostnames (as SLURM doesn't like FQDNs).
    
     - Currently storing state in data bags, but should it
    be in attributes instead?
    
    Didn't commit as atomically as would
    have been nice :-(, but I got caught up in troubleshooting.

--- cookbooks/slurm-mpi-cluster/recipes/slurm.rb
+++ cookbooks/slurm-mpi-cluster/recipes/slurm.rb
@@ -1,18 +1,59 @@
+# Get data bag configuration
 slurm = data_bag_item("slurm", "config")
+mkey = data_bag_item("slurm","munge-key")
 
+# Install packages
 %w{munge slurm-llnl slurm-llnl-torque slurm-llnl-basic-plugins slurm-llnl-basic-plugins-dev}.each do |pkg|
     package pkg do
         action [:install]
     end
 end
 
-user(slurm['user'])
-
-template "/etc/slurm/slurm.conf" do
-    source "slurm.conf.erb"
-    variables( :master => slurm['master'], :computes => slurm['computes'], :part_name => slurm['part_name'], :cpus => slurm['cpus'] )
+# munge configuration
+directory "/etc/munge" do
+    action :create
 end
 
-service "slurm" do
+directory "/etc" do
+    mode "0755"
+end
+
+user("munge")
+
+template "/etc/munge/munge.key" do
+    source "munge.key.erb"
+    owner "munge"
+    variables(:key => mkey['key'])
+end
+
+service "munge" do
+    action [:enable,:start]
+end
+
+# slurm configuration
+user(slurm['user'])
+
+directory "/etc/slurm-llnl" do 
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+end
+
+template "/etc/slurm-llnl/slurm.conf" do
+    source "slurm.conf.erb"
+    owner "slurm"
+    mode "0755"
+    variables( 
+        :master => slurm['master'], 
+        :master_addr => slurm['master_addr'], 
+        :computes => slurm['computes'].join(','), 
+        :compute_addrs => slurm['compute_addrs'].join(','), 
+        :part_name => slurm['part_name'], 
+        :cpus => slurm['cpus'] 
+    )
+end
+
+service "slurm-llnl" do
     action [:enable,:start]
 end
