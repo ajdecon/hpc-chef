4d3713bc2f3fd8c7bb3756196e26a32a36113d61
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-19 17:58:06 -0600

    Modified ec2 environment for a test

--- environments/ec2test.rb
+++ environments/ec2test.rb
@@ -1,7 +1,7 @@
-name "ec2slurm-test"
+name "ec2"
 description "An environment for testing the slurm cookbook on ec2"
-override_attributes "slurm" => { 'master' => 'slmaster', 'master_addr' => '10.0.0.1',
-    'computes' => ['node01','node02'], 'compute_addrs' => ['10.0.1.1', '10.0.1.2'],
-    'cpus' => '8' }, "nfs" => { 'headnode_addr' => '10.0.0.1', 'shared_dirs' => ['/scratch','/test'],
+override_attributes "slurm" => { 'master' => 'domU-12-31-39-00-A4-87', 'master_addr' => '10.254.171.117',
+    'computes' => ['domU-12-31-39-16-C9-DB','domU-12-31-39-13-D5-27'], 'compute_addrs' => ['10.96.202.41', '10.201.214.213'],
+    'cpus' => '1' }, "nfs" => { 'headnode_addr' => '10.254.171.117', 'shared_dirs' => ['/scratch'],
     'shared_perms' => ['0777','0755'],
-    'clients' => ['10.0.1.1','10.0.1.2'] }
+    'clients' => ['10.96.202.41','10.201.214.213'] }
