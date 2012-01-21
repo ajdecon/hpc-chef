9e53761f4aeb7c698676004eca9a9f8dc35882d5
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-21 11:25:20 -0600

    Add comments to slurm recipe

--- cookbooks/slurm-mpi-cluster/recipes/slurm.rb
+++ cookbooks/slurm-mpi-cluster/recipes/slurm.rb
@@ -5,29 +5,39 @@
     end
 end
 
-# munge configuration
+# Munge is used by SLURM for authentication, and requires a few 
+# pre-conditions.
+
+# Make sure /etc/munge directory exists
 directory "/etc/munge" do
     action :create
 end
 
+# Make /etc have suitable permissions
 directory "/etc" do
     mode "0755"
 end
 
+# Make sure the munge user exists
 user("munge")
 
+# Create the munge key from template
 template "/etc/munge/munge.key" do
     source "munge.key.erb"
     owner "munge"
 end
 
+# Enable and start the munge service
 service "munge" do
     action [:enable,:start]
 end
 
-# slurm configuration
+# SLURM configuration
+
+# Create the slurm user based on settings
 user(node.slurm['user'])
 
+# Make sure the config directory exists
 directory "/etc/slurm-llnl" do 
     owner "root"
     group "root"
@@ -35,12 +45,14 @@ directory "/etc/slurm-llnl" do
     action :create
 end
 
+# Build slurm.conf based on the template
 template "/etc/slurm-llnl/slurm.conf" do
     source "slurm.conf.erb"
     owner "slurm"
     mode "0755"
 end
 
+# Enable and start the slurm service
 service "slurm-llnl" do
     action [:enable,:start]
 end
