4a80d92c21047a44057157af8a40b538f72daff3
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-21 15:17:48 -0600

    Default cookbook attributes for SLURM are more generic

--- cookbooks/slurm-mpi-cluster/attributes/default.rb
+++ cookbooks/slurm-mpi-cluster/attributes/default.rb
@@ -1,14 +1,14 @@
 # SLURM attributes
-default['slurm']['master']         = "ip-10-85-2-131"
-default['slurm']['master_addr']    = "107.21.148.168"
-default['slurm']['computes']       = [ "ip-10-144-65-60", "ip-10-36-79-48" ]
-default['slurm']['compute_addrs']  = [ "107.22.142.152", "107.22.136.212" ]
-default['slurm']['part_name']      = "debug"
+default['slurm']['master']         = "slmaster"
+default['slurm']['master_addr']    = "10.0.0.1"
+default['slurm']['computes']       = [ "compute1", "compute2" ]
+default['slurm']['compute_addrs']  = [ "10.0.1.1", "10.0.1.2" ]
+default['slurm']['part_name']      = "production"
 default['slurm']['user']           = "slurm"
 default['slurm']['cpus']           = "1"
 
 # MUNGE attributes
-default['munge']['key']    = "nfusdnfjkaniuownfnljdnvcuowanrljvnjnvuoandfnsjanvuonejlnvjnuoannfdjslnvuaownljnfsdljvnsdualnfvjnafsuvnoweiunvlgrn"
+default['munge']['key']    = "RandomKeyGoesHereRandomKeyGoesHereRandomKeyGoesHereRandomKeyGoesHere"
 
 # NFS share attributes
 default['nfs']['headnode_addr']    = default['slurm']['master_addr']
